67c2c48639ca16458c5e90b4eac8f88d2800efc8
Taehwan2 <taehwansw@gmail.com>
2023-06-12 14:09:59 +0900

    commit

---
+++ src/main/java/com/example/gitpratciec/GitpratciecApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpratciec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpratciecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpratciecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitpratciec/GitpratciecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpratciec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpratciecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
